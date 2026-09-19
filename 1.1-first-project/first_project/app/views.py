a8e7df226f9648f2680adab4e2a260565844094f
A-leks-andr <calllamurzakov@gmail.com>
2026-09-19 13:17:11 +0300

    fixed with ruff

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,9 +1,9 @@
-import datetime
 import os
 
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils import timezone
 
 
 def home_view(request):
@@ -16,23 +16,39 @@ def home_view(request):
         "Показать содержимое рабочей директории": reverse("workdir"),
     }
 
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
     context = {"pages": pages}
     return render(request, template_name, context)
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет,
-    # возвращается просто текст
-    current_time = datetime.datetime.now().time()
-    formatted_time = current_time.strftime("%H:%M:%S")
-    msg = f"Текущее время: {formatted_time}"
-    return HttpResponse(msg)
+    # обратите внимание – здесь HTML шаблона нет, возвращается просто текст
+    home_url = reverse("home")
+    local_time = timezone.localtime(timezone.now())
+    formatted_time = local_time.strftime("%H:%M:%S")
+    back = f'Вернуться на <a href="{home_url}">главная</a> страницу'
+    msg = f"Страница загрузилась в: {formatted_time}"
+
+    msg_2 = (
+        'Ваше время: <span id="local-time">Загрузка...</span>'
+        "<script>"
+        "  function updateTime() {"
+        "    const now = new Date();"
+        '    const timeString = now.toTimeString().split(" ")[0];'  # формат HH:MM:SS
+        '    document.getElementById("local-time").innerText = timeString;'
+        "  }"
+        "  updateTime();"  # Запускаем сразу при загрузке
+        "  setInterval(updateTime, 1000);"  # Обновляем каждую секунду
+        "</script>"
+    )
+    resp_html = f"{back}<br><br>{msg}<br><br>{msg_2}"
+    return HttpResponse(resp_html)
 
 
 def workdir_view(request):
+    home_url = reverse("home")
+    back = f'Вернуться на <a href="{home_url}">главная</a> страницу'
     work_dir = os.getcwd()
     files = os.listdir(work_dir)
     files_text = "<br>".join(f"- {file}" for file in files)
-    return HttpResponse(f"Файлы в рабочей директории: <br><br>{files_text}")
+    resp_html = f"{back}<br><br>Файлы в рабоче директории:<br><br>{files_text}"
+    return HttpResponse(resp_html)
