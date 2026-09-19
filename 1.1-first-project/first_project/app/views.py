b324134f702cc83b84a8717533820c001ca74c13
A-leks-andr <calllamurzakov@gmail.com>
2026-09-19 14:52:06 +0300

    adding currency conversion and about the project

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -5,6 +5,15 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.utils import timezone
 
+EXCHANGE_RATES = {
+    "USD_RUB": 84.1975,
+    "EUR_RUB": 96.6671,
+    "USD_EUR": 0.86,
+    "EUR_USD": 1.15,
+    "RUB_USD": 0.0118,
+    "RUB_EUR": 0.0103,
+}
+
 
 def home_view(request):
     template_name = "app/home.html"
@@ -14,6 +23,8 @@ def home_view(request):
         "Главная страница": reverse("home"),
         "Показать текущее время": reverse("time"),
         "Показать содержимое рабочей директории": reverse("workdir"),
+        "О проекте": reverse("about"),
+        "Конвертация валют": reverse("convert"),
     }
 
     context = {"pages": pages}
@@ -51,4 +62,54 @@ def workdir_view(request):
     files = os.listdir(work_dir)
     files_text = "<br>".join(f"- {file}" for file in files)
     resp_html = f"{back}<br><br>Файлы в рабоче директории:<br><br>{files_text}"
+    return HttpResponse(resp_html)
+
+
+def about_view(request):
+    home_url = reverse("home")
+    convert_url = reverse("convert")
+    back = f'Вернуться на <a href="{home_url}">главная</a> страницу'
+    msg = (
+        f"О проекте:<br><br>"
+        f"Это простое веб-приложение для конвертации валют.<br>"
+        f"Как пользоваться:<br>"
+        f"1. Перейдите на страницу <a href={convert_url}>/convert/</a><br>"
+        f"2. Добавьте параметры в URL:<br>"
+        f"?from=USD&to=RUB&amount=100<br>"
+        f"Доступные валюты: USD, EUR, RUB."
+    )
+
+    resp_html = f"{back}<br><br>{msg}"
+    return HttpResponse(resp_html)
+
+
+def convert_view(request):
+    home_url = reverse("home")
+    back = f'Вернуться на <a href="{home_url}">главную</a> страницу'
+    info = "Информация о курсе на 19 сентября 2026 года"
+
+    if request.GET:
+        from_currency = request.GET.get("from")
+        to_currency = request.GET.get("to")
+        amount = request.GET.get("amount")
+
+        curs = f"{from_currency}_{to_currency}"
+        key = [k for k in EXCHANGE_RATES.keys()]
+        if curs not in key:
+            msg = f"Курс для пары {from_currency} → {to_currency} не найден"
+
+        elif not amount.isdigit():
+            msg = f"amount {amount} не является числом"
+
+        else:
+            result = EXCHANGE_RATES[curs] * float(amount)
+            msg = f"{amount} {from_currency} = {result:.2f} {to_currency}"
+    else:
+        msg = (
+            "Вы перешли на страницу конвертера без параметров.<br><br>"
+            "Пожалуйста, укажите параметры: from, to, amount<br><br>"
+            "Пример: /convert/?from=USD&to=RUB&amount=10"
+        )
+
+    resp_html = f"{back}<br><br>{info}<br><br>{msg}"
     return HttpResponse(resp_html)
--- 1.1-first-project/first_project/first_project/urls.py
+++ 1.1-first-project/first_project/first_project/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from app.views import home_view, time_view, workdir_view
+from app.views import about_view, convert_view, home_view, time_view, workdir_view
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("", home_view, name="home"),
-    # Раскомментируйте код, чтобы данные урлы
-    # обрабатывались Django
     path("current_time/", time_view, name="time"),
     path("workdir/", workdir_view, name="workdir"),
+    path("about/", about_view, name="about"),
+    path("convert/", convert_view, name="convert"),
     path("admin/", admin.site.urls),
 ]
